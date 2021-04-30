258d535615e40ef7a2225c0bac29f1a5119f781a
Isabella <wileyisabella@gmail.com>
2021-04-30 12:19:46 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else 
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else 
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else 
+      false
+    end
   end
 
   def count_sentences
-
+    delimeter = Regexp.union(".", "?", "!")
+    arr = self.split(delimeter)
+    arr.delete("")
+    arr.length
   end
 end
